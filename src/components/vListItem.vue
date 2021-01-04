6e25a656f31ba0c71fd0f1ace38824f0eecb691c
lj <1153558131@qq.com>
2021-01-04 16:07:47 +0800

    新增已读文章 列表项变透明 优化了一下代码

--- src/components/vListItem.vue
+++ src/components/vListItem.vue
@@ -1,8 +1,11 @@
 <template>
   <router-link class="a" :to="'/info/'+data.id">
-    <div  :class="{active:data.isRead,item:true}">
+    <div :class="{active:data.isRead,item:true}">
       <div class="left" :style="{paddingTop:data.title.length>14?0:'16px'}">
-        <h3>{{data.title}}</h3>
+        <h3 class="van-multi-ellipsis--l2">
+            {{ data.title }}
+        </h3>
+
         <p>{{ data.hint }}</p>
       </div>
       <div class="right">
@@ -23,40 +26,45 @@ import {mapActions} from "vuex";
 
 export default {
   name: "vListItem",
-  props:["data"]
+  props: ["data"]
 }
 </script>
 
 <style scoped>
-  .item{
-    height: 110px;
-    padding:16px 0;
-    display: flex;
-    justify-content: space-between;
-    box-sizing: border-box;
-  }
-  .item .left{
-    width: 248px;
-  }
-  .item .left h3{
-    font-size: 18px;
-    font-weight: bold;
-    line-height: 25px;
-    color: var(--font-color-1);
-  }
-  .item .left p{
-    font-size: 10px;
-    color: var(--font-color-2);
-    line-height: 20px;
-  }
-  .item .right >>>.img{
-    height: 76px !important;
-    width: 76px !important;
-    border-radius: 5px;
-    overflow: hidden;
-  }
-  .active {
-    opacity: .5;
-  }
+.item {
+  height: 110px;
+  padding: 16px 0;
+  display: flex;
+  justify-content: space-between;
+  box-sizing: border-box;
+}
+
+.item .left {
+  width: 248px;
+}
+
+.item .left h3 {
+  font-size: 18px;
+  font-weight: bold;
+  line-height: 25px;
+  color: var(--font-color-1);
+}
+
+.item .left p {
+  font-size: 10px;
+  color: var(--font-color-2);
+  line-height: 20px;
+}
+
+.item .right >>> .img {
+  height: 76px !important;
+  width: 76px !important;
+  border-radius: 5px;
+  overflow: hidden;
+}
+
+.active {
+  opacity: .5;
+}
 
 </style>
